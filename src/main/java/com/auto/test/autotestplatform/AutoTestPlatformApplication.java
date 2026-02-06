c57db000f722a54e6304866bafe91f24473fee92
夏日 <lindsay@xiaridemacbook-air.local>
2026-02-06 14:49:57 +0800

    init: 项目初始化，创建IDEA项目+提交第1天规划成果（修正report_data表默认值）

---
+++ src/main/java/com/auto/test/autotestplatform/AutoTestPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.auto.test.autotestplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTestPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTestPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auto/test/autotestplatform/AutoTestPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auto.test.autotestplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTestPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
